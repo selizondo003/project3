c9f94ade58f72393e59b744d7be3cc90d78e587a
sofia <selizondo003@insite.4cd.edu>
2022-04-29 13:59:51 -0700

    working section buttons

--- j/main.js
+++ j/main.js
@@ -1,126 +1,105 @@
 import {
     cueTimer
 } from "./modules/cuepoints.js";
-const vid = document.querySelector('.whale');
 
-//video control buttons
-const myplay = document.getElementById('myplay');
-const mypause = document.getElementById('mypause');
-const mymute = document.getElementById('mymute');
-const myunmute = document.getElementById('myunmute');
-
-const selectVid = document.querySelector("#video_select");
-const selectTxt = document.querySelector("#text-track");
-const display = document.getElementById("transcript");
-const transcript_en = document.getElementById("transcript-en");
-const showHide = document.getElementById("show-hide");
-
-
-
-//section list
-const saveMovement = document.getElementById('saveMovement');
-const ecoEngineer = document.getElementById('ecoEngineer');
-const whalePoop = document.getElementById('whalePoop');
-const whaleCarcas = document.getElementById('whaleCarcas');
-
-
-vid.src = "assets/vosWhale.mp4";
-vid.load();
-
-//add event listeners for control
-
-myplay.addEventListener('click', (e) => {
-    playVideo(vid);
-});
-
-mypause.addEventListener('click', (e) => {
-    pauseVideo(vid);
-});
-
-mymute.addEventListener('click', (e) => {
-    muteVid(vid);
-});
-
-myunmute.addEventListener('click', (e) => {
-    unmuteVid(vid);
-});
-
-selectVid.addEventListener("change", (e) => {
-    selectVideo(e, vid);
-});
-
-selectTxt.addEventListener("change", (e) => {
-    const id = e.target.value;
-    selectTrack(e, vid, id);
-});
-
-transcript_en.addEventListener(
-    "click",
-    function (e) {
-        e.preventDefault();
-        webvttTranscript("captions/synergy.vtt", display);
-    });
-    showHide.addEventListener(
-        "click",
-        function (e) {
-            e.preventDefault();
-            webvttTranscript("subtitles/french.vtt", display);
-            if (e.target.innerHTML == "Show Transcript") {
-                e.target.innerHTML = "Hide Transcript";
-                display.style.display = "block";
-            } else {
-                e.target.innerHTML = "Show Transcript";
-                display.style.display = "none";
-            }
-        });
-
-//creating certain sections in video 
-saveMovement.addEventListener('click', (e) => {
-    vid.currentTime = 42;
-});
-ecoEngineer.addEventListener('click', (e) => {
-    vid.currentTime = 93;
-});
-whalePoop.addEventListener('click', (e) => {
-    vid.currentTime = 124.12;
-});
-whaleCarcas.addEventListener('click', (e) => {
-    vid.currentTime = 189.50;
-});
-
-
-setTimeout(function () {
-    console.log("Ready")
-}, 5);
 
 document.addEventListener("DOMContentLoaded", (e) => {
 
     var myCues = [
 
-        {
-            seconds: 11,
-            callback: bG
-        },
-        {
-            seconds: 2,
-            callback: image
-        },
-        {
-            seconds: 97,
-            callback: engineer
-        },
-        {
-            seconds: 124.12,
-            callback: feces
-        }
-
-
+        {seconds: 11,callback: bG},
+        {seconds: 2,callback: image},
+        {seconds: 97, callback: engineer},
+        {seconds: 152, callback: feces}
     ];
 
     //this activates the cuepoints module.
     // Pass it the ID of the video to watch
     // and the array of cuepoint objects.
     cueTimer.setup("vid", myCues);
+
+    const vid = document.querySelector('.whale');
+
+    const selectTxt = document.querySelector("#text-track");
+    const display = document.getElementById("transcript");
+    const transcript_en = document.getElementById("transcript-en");
+    const transcript_es = document.getElementById("transcript-es");
+    const transcript_fr = document.getElementById("transcript-fr");
+    const showHide = document.getElementById("show-hide");
+
+
+
+    //section list
+    const saveMovement = document.getElementById('saveMovement');
+    const ecoEngineer = document.getElementById('ecoEngineer');
+    const whalePoop = document.getElementById('whalePoop');
+    const whaleCarcas = document.getElementById('whaleCarcas');
+
+
+    vid.src = "assets/vosWhale.mp4";
+    vid.load();
+
+
+    
+
+    selectTxt.addEventListener("change", (e) => {
+        const id = e.target.value;
+        selectTrack(e, vid, id);
+    });
+
+    transcript_en.addEventListener(
+        "click",
+        function (e) {
+            e.preventDefault();
+            webvttTranscript("captions/whale.vtt", display);
+        });
+
+    transcript_es.addEventListener(
+        "click",
+         function (e) {
+            e.preventDefault();
+            webvttTranscript("subtitles/spanish.vtt", display);
+        });
+    
+    transcript_fr.addEventListener(
+        "click",
+        function (e) {
+            e.preventDefault();
+            webvttTranscript("subtitles/french.vtt", display);
+        });
+        
+    showHide.addEventListener(
+         "click",
+        function (e) {
+            e.preventDefault();
+            webvttTranscript("captions/whale.vtt", display);
+                if (e.target.innerHTML == "Show Transcript") {
+                    e.target.innerHTML = "Hide Transcript";
+                    display.style.display = "block";
+                } else {
+                    e.target.innerHTML = "Show Transcript";
+                    display.style.display = "none";
+                }
+            });
+
+    //creating certain sections in video 
+    saveMovement.addEventListener('click', (e) => {
+        vid.currentTime = 42;
+    });
+    ecoEngineer.addEventListener('click', (e) => {
+        vid.currentTime = 93;
+    });
+    whalePoop.addEventListener('click', (e) => {
+        vid.currentTime = 124.12;
+    });
+    whaleCarcas.addEventListener('click', (e) => {
+        vid.currentTime = 189.50;
+    });
+
+
+    setTimeout(function () {
+        console.log("Ready")
+    }, 5);
 
 
 });
@@ -137,7 +116,7 @@ function bG() {
 
 function image() {
     
-    document.getElementById("web").src = "../project3/images/imgWhale.jpg";
+    document.getElementById("web").src = "images/imgWhale.jpg";
 };
 
 function engineer() {
@@ -146,7 +125,7 @@ function engineer() {
 };
 
 function feces() {
-    document.getElementById("web").src = "/images/WhalePump.jpg";
+    document.getElementById("web").src = "images/WhalePump.jpg";
     pauseVideo(vid);
     alert("Click play after looking over document :)")
 
